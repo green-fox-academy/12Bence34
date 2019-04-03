c1bb54e387bca01ce2cf9084bb116dd409c2cddd
12Bence34 <vbence1991@gmail.com>
2019-04-03 14:26:30 +0200

    define_basic_info

---
+++ week-01/day-3/define_basic_info/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Define several things as a variable then print their values
+    // Your name as a string
+    // Your age as an integer
+    // Your height in meters as a double
+    // Whether you are married or not as a boolean
+
+    std::string name = "Bence";
+    std::cout<< name <<std:: endl;
+
+    int  age;
+    age= 27;
+    std:: cout<<age<<std:: endl;
+
+    double height;
+    height=1.89;
+    std::cout<<height<< "m"<<std:: endl;
+
+    bool married = false;
+    std::cout<< married <<std:: endl;
+
+
+
+
+
+
+    return 0;
+}
