412bde8b673bb8147692b2be8ce88ccd50890444
12Bence34 <vbence1991@gmail.com>
2019-04-03 12:47:28 +0200

    Hello_Others

---
+++ week-01/day-3/Hello_Others/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    std:: cout << "Hello, Alex" << std:: endl;
+    std:: cout << "Hello, Julcsi" << std:: endl;
+    std:: cout << "Hello, Anna" << std:: endl;
+    return 0;
+}
