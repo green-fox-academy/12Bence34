6f4335f11d847f3ceae02bcafeacab97d5170581
12Bence34 <ezmegazbence@gmail.com>
2019-04-11 11:21:24 +0200

    Number-adder done

---
+++ week-02/day-4/number-adder/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void Addnumber(int n);
+
+int main() {
+
+    //Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+    int n;
+
+    std::cout << "Type a number please:" << std::endl;
+    std::cin >> n;
+    std::cout << " " << std::endl;
+
+    Addnumber(n);
+
+    return 0;
+}
+
+void Addnumber(int n) {
+    if (n < 1)
+        return;
+    Addnumber(n - 1);
+    std::cout << n << std::endl;
+
+}
